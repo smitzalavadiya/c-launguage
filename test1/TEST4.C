1dd0e96a17bd48f9a5de682ca321c63af42ba08b
RomilVasoya <romil.vasoya22@gmail.com>
2022-12-26 16:38:53 +0530

    exam

---
+++ test1/TEST4.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+	  int a,b,c;
+	  clrscr();
+	  printf("enter value of a and b =",a,b);
+	  scanf("%d %d",&a,&b);
+	  c=a;
+	  a=b;
+	  b=c;
+	  printf("\n a:%d and b:%d",a,b) ;
+
+
+
+
+      getch();
+     }
---
+++ test1/TEST5.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+main(){
+   float  f,c=12;
+
+   clrscr();
+   printf("enter celsius:",f=(c*1.8)+32);
+   scanf("%d",&c);
+   printf("f:%d",f);
+
+
+
+
+
+
+  getch();
+}
